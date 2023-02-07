bb7a6e4534b67b7b81a050775dc6334cc0d25842
Nikita Chukarev <nikita.chukarev@bravedevelopers.com>
2023-02-07 18:04:53 +0400

    add transparent system status bar

--- lib/internal/app.dart
+++ lib/internal/app.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:rick_and_morty/navigation/app_router.dart';
 
 class App extends StatefulWidget {
@@ -19,24 +20,43 @@ class _AppState extends State<App> {
   Widget build(BuildContext context) {
     return AdaptiveTheme(
       light: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.light,
-        colorSchemeSeed: const Color.fromRGBO(108, 81, 149, 1),
-      ),
+          useMaterial3: true,
+          brightness: Brightness.light,
+          colorSchemeSeed: const Color.fromRGBO(108, 81, 149, 1),
+          appBarTheme: AppBarTheme(
+            systemOverlayStyle: SystemUiOverlayStyle.dark.copyWith(
+              statusBarColor: Colors.transparent,
+            ),
+          )),
       dark: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.dark,
-        colorSchemeSeed: const Color.fromRGBO(108, 81, 149, 1),
-      ),
+          useMaterial3: true,
+          brightness: Brightness.dark,
+          colorSchemeSeed: const Color.fromRGBO(108, 81, 149, 1),
+          appBarTheme: AppBarTheme(
+            systemOverlayStyle: SystemUiOverlayStyle.light.copyWith(
+              statusBarColor: Colors.transparent,
+            ),
+          )),
       initial: widget.themeMode ?? AdaptiveThemeMode.system,
-      builder: (theme, darkTheme) => MaterialApp.router(
-        routerDelegate:
-            router.delegate(navigatorObservers: () => [MyObserver()]),
-        routeInformationParser: router.defaultRouteParser(),
-        title: 'Rick and Morty',
-        theme: theme,
-        darkTheme: darkTheme,
-      ),
+      builder: (theme, darkTheme) {
+        SystemChrome.setSystemUIOverlayStyle(
+          theme.appBarTheme.systemOverlayStyle!.copyWith(
+            systemNavigationBarColor: Color.alphaBlend(
+              theme.colorScheme.background.withOpacity(.7),
+              theme.colorScheme.primary,
+            ),
+          ),
+        );
+
+        return MaterialApp.router(
+          routerDelegate:
+              router.delegate(navigatorObservers: () => [MyObserver()]),
+          routeInformationParser: router.defaultRouteParser(),
+          title: 'Rick and Morty',
+          theme: theme,
+          darkTheme: darkTheme,
+        );
+      },
     );
   }
 }
